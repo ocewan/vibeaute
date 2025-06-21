0b6de6d46bf0df3c52db1fca5a37b72fe2a40480
ocewan <ocebrii@gmail.com>
2025-06-21 16:42:05 +0200

    Récupération de la bonne route et exceptions

--- src/Routing/Router.php
+++ src/Routing/Router.php
@@ -14,10 +14,26 @@ class Router
 
     public function handleRequest (string $uri)
     {
+        try{
         $path = $this->normalizePath($uri);
+        if (!isset($this->routes[$path])) {
+            // Gérer le cas où la route n'existe pas en lançant une exception
+            throw new \Exception("Route non trouvée pour le chemin: $path");
+        }
+        $route = $this->routes[$path];
+
+        $controllerPath = $route['controller'];
+        $action = $route['action'];
+
+        $controller = new $controllerPath();
+        $controller->$action();
+        } catch (\Exception $e) {
+            // Gérer l'erreur en affichant un message d'erreur
+            echo $e->getMessage();
+        }
     }
 
-    public static function normalizePath(string $path) : string
+    public static function normalizePath(string $uri) : string
     {
         // Normalise le chemin en supprimant les paramètres et en ajoutant un slash final
         $path = parse_url($uri, PHP_URL_PATH);
