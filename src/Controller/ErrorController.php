8fb7d4d94ae0c2105225a6dddccd640b1a0ef655
ocewan <ocebrii@gmail.com>
2025-06-24 21:37:45 +0200

    Création du controleur pour les messages d'erreurs

---
+++ src/Controller/ErrorController.php
@@ -0,0 +1,15 @@
+<?php 
+
+namespace App\Controller;
+
+class ErrorController extends Controller
+{ 
+
+    public function show(string $errorMessage) : void
+    {
+        // On utilise la méthode render de la classe Controller pour afficher le template d'erreur
+        $this->render("errors/default", [
+            "errorMessage" => $errorMessage
+        ]);
+    }
+}
