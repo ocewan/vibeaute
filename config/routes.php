1edbca195f1911bf50e0f4e604bf6303bfd4c379
ocewan <ocebrii@gmail.com>
2025-06-24 21:44:16 +0200

    Ajout de routes supplémentaires

--- config/routes.php
+++ config/routes.php
@@ -1,5 +1,7 @@
 <?php
 return [
     "/" => ["controller" => "App\Controller\PageController", "action" => "home"],
+    "/prestation/" => ["controller" => "App\Controller\PageController", "action" => "prestation"],
+    "/tarif/" => ["controller" => "App\Controller\PageController", "action" => "tarif"],
     "/contact/" => ["controller" => "App\Controller\PageController", "action" => "contact"],
 ];
