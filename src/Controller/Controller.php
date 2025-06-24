c62277953284a00707d3fb47c546626dcd56d623
ocewan <ocebrii@gmail.com>
2025-06-24 21:36:58 +0200

    Ajout du paramètre de tableau

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -4,7 +4,7 @@ namespace App\Controller;
 
 class Controller 
 {
-        protected function render(string $path) : void
+        protected function render(string $path, array $params = []) : void
     {
         // définition de la variable $filePath pour le chemin du fichier de template
         $filePath = APP_ROOT . "/templates/$path.php";
@@ -12,6 +12,8 @@ class Controller
         if (!file_exists ($filePath)) {
             echo "Fichier n'existe pas: $filePath";
         } else {
+            // transforme chaque clé du tableau en variable
+            extract ($params);
             require_once $filePath;
         }
 
