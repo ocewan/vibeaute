dd0d0464acc31c7ed23a9629664742e3944bc1a4
ocewan <ocebrii@gmail.com>
2025-06-25 16:42:04 +0200

    Ajout des propriétés, getters et setters

--- src/Entity/Prestation.php
+++ src/Entity/Prestation.php
@@ -1,8 +1,62 @@
-<?php 
+<?php
 
 namespace App\Entity;
 
-class Prestation 
+class Prestation
 {
-    protected
+    protected ?int $id = null;
+    protected ?string $category = null;
+    protected ?string $title = null;
+    protected ?float $price = null;
+    protected \DateTime $createdAt = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(?int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getCategory(): ?string
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?string $category): void
+    {
+        $this->category = $category;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(?string $title): void
+    {
+        $this->title = $title;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(?float $price): void
+    {
+        $this->price = $price;
+    }
+
+    public function getCreatedAt(): ?\DateTime
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(?\DateTime $createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
 }
