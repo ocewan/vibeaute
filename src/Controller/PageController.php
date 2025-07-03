1285da5fd9b08b5d95685a49356cdc38e57bc610
ocewan <ocebrii@gmail.com>
2025-07-03 19:26:23 +0200

    Ajout de l'action Tarifs

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -1,18 +1,26 @@
-<?php 
+<?php
 
 namespace App\Controller;
 
-class PageController extends Controller
-{ 
+use App\Repository\TarifRepository;
 
-    public function home() : void
+class PageController extends Controller
+{
+
+    public function home(): void
     {
         $this->render("pages/home");
+    }
+
+    public function tarifs(): void
+    {
+        $tarifRepository = new TarifRepository();
+        $tarifs = $tarifRepository->getAllTarifs();
+        $this->render("pages/tarifs", ["tarifs" => $tarifs]);
     }
 
     public function contact()
     {
         echo "<h1>Contactez nous !</h1>";
     }
-
 }
