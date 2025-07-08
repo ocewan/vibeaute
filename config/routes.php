338001f67a757d578e2a8e27f83d169798b4af8a
ocewan <ocebrii@gmail.com>
2025-07-08 21:26:09 +0200

    Ajout de la route login

--- config/routes.php
+++ config/routes.php
@@ -4,4 +4,5 @@ return [
     "/prestations/" => ["controller" => "App\Controller\PageController", "action" => "prestations"],
     "/tarifs/" => ["controller" => "App\Controller\PageController", "action" => "tarifs"],
     "/contact/" => ["controller" => "App\Controller\PageController", "action" => "contact"],
+    "/login/" => ["controller" => "App\Controller\PageController", "action" => "login"],
 ];
