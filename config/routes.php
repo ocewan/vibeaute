32eca9029bef291183582bf3473f4c58c293ac68
ocewan <ocebrii@gmail.com>
2025-07-09 19:11:47 +0200

    Ajout des différentes routes pour les pages et actions du backoffice

--- config/routes.php
+++ config/routes.php
@@ -5,4 +5,41 @@ return [
     "/tarifs/" => ["controller" => "App\Controller\PageController", "action" => "tarifs"],
     "/contact/" => ["controller" => "App\Controller\PageController", "action" => "contact"],
     "/login/" => ["controller" => "App\Controller\PageController", "action" => "login"],
+    "/logout/" => ["controller" => "App\Controller\PageController", "action" => "logout"],
+    "/dashboard/" => ["controller" => "App\Controller\PageController", "action" => "dashboard"],
+
+    "/admin/add-tarif/" => [
+        "controller" => "App\Controller\Controller",
+        "action" => "addTarif"
+    ],
+    "/admin/update-tarif/" => [
+        "controller" => "App\Controller\Controller",
+        "action" => "updateTarif"
+    ],
+    "/admin/delete-tarif/" => [
+        "controller" => "App\Controller\Controller",
+        "action" => "deleteTarif"
+    ],
+    "/contact-submit/" => [
+        "controller" => "App\Controller\Controller",
+        "action" => "submitContactForm"
+    ],
+    "/admin/mark-message-read/" => [
+        "controller" => "App\Controller\Controller",
+        "action" => "markMessageAsRead"
+    ],
+    "/admin/delete-message/" => [
+        "controller" => "App\Controller\Controller",
+        "action" => "deleteMessage"
+    ],
+    "/admin/upload-photo/" => [
+        "controller" => "App\Controller\Controller",
+        "action" => "uploadPhoto"
+    ],
+    "/admin/delete-photo/" => [
+        "controller" => "App\Controller\Controller",
+        "action" => "deletePhoto"
+    ],
+
+
 ];
